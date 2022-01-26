9355dc61c85da2e9a2f4ca4726de39ab58ca90a5
bayezid mir <warner.roman@gmail.com>
2022-01-26 12:31:28 +0600

    added notes and practices

---
+++ Day-17/practice.js
@@ -0,0 +1,34 @@
+// number
+var applePrice = 230;
+var appleQuantity = 19;
+var people = 10;
+
+// mathematical operation
+var totalApplePrice = applePrice * appleQuantity;
+console.log(totalApplePrice);
+
+// string
+var firstName = "bayezid";
+var secondName = "mir";
+var fullName = firstName.concat(" ", secondName);
+console.log(fullName);
+
+// boolean
+var honesty = true;
+var courage = false;
+
+// typeof
+console.log(typeof honesty);
+
+// conversion
+var num1 = "99.5";
+num1 = parseFloat(num1);
+var num2 = 15;
+console.log(typeof num2);
+
+var dollarExchangeRate = 84.662325;
+var dollarRate = parseFloat(dollarExchangeRate.toFixed(2));
+console.log(dollarRate);
+
+// remainder
+console.log(appleQuantity % people);

e309e06f47cdd1a4d1fbed6cb208ebda28a3ab10
bayezid mir <warner.roman@gmail.com>
2022-02-21 23:01:26 +0600

    fetching data and showing to our website

--- Day-33/API.js
+++ Day-33/API.js
@@ -9,5 +9,24 @@ function fetchingData() {
     .then((data) => console.log(data));
 }
 
-//https://jsonplaceholder.typicode.com/users
-//https://jsonplaceholder.typicode.com/posts
+function userPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+
+// Fetching data from Server and showing to UI
+function userData() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.json())
+    .then((data) => showingDataToUI(data));
+}
+
+function showingDataToUI(data) {
+  const users = document.getElementById("users-container");
+  for (const user of data) {
+    const li = document.createElement("li");
+    li.innerText = `Name: ${user.name} , Email: ${user.email}`;
+    users.appendChild(li);
+  }
+}
